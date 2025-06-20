7fea304601fa59f5c6fda04f03c350c5d22c33cb
zhufucdev <zhufuzhufu1@gmail.com>
2025-06-20 13:27:06 +0800

    format imports

--- model.py
+++ model.py
@@ -1,10 +1,9 @@
-from torch import nn
-from torch.nn import functional as F
-from torch.utils.data import Dataset
-import torch
 import json
 from os import PathLike
 
+import torch
+from torch import nn
+from torch.utils.data import Dataset
 from transformers import AutoModel, BitsAndBytesConfig
 
 
